5d8ee3813b94c70c997d3c0a9f408c1757e4f627
Chenxi <lin.chenxi14@gmail.com>
2024-02-22 19:29:49 -0500

    updated home component with tablet view

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import styles from './Home.module.scss'
 export default function Home() {
     return (
         <section className={styles.container}>
-            <p>SO, YOU WANT TO TRAVEL TO</p>
+            <p className={styles.heading2}>SO, YOU WANT TO TRAVEL TO</p>
             <h1 className={styles.heading1}>SPACE</h1>
             <p className={styles.body}>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             <a href="">
