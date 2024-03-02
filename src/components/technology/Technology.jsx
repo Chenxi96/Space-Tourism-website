d2992ee45832aacc1457a2cd619c5c4bf5747edc
Chenxi <lin.chenxi14@gmail.com>
2024-03-02 00:01:26 -0500

    added a route and component for technology

---
+++ src/components/technology/Technology.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom"
+
+export default function Technology() {
+    return(
+        <div>
+            <p><span>03</span>SPACE LAUNCH 101</p>
+            <div><img src='' alt="" /></div>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link>1</Link>
+                        </li>
+                        <li>
+                            <Link>2</Link>
+                        </li>
+                        <li>
+                            <Link>3</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div>
+                    <p></p>
+                    <h2></h2>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    )
+}
