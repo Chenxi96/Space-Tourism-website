a731e17a684b095d7a94ad9832acfc52688ba271
Chenxi <lin.chenxi14@gmail.com>
2024-03-03 20:24:57 -0500

    styled the technology component mobile

--- src/components/technology/Technology.jsx
+++ src/components/technology/Technology.jsx
@@ -1,28 +1,40 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import styles from './Technology.module.scss'
 
 export default function Technology() {
+    const [technology, setTechology] = useState({});
+
+    useEffect(() => {
+        fetch('../../../data.json')
+            .then(res => res.json())
+            .then(data => setTechology(data.technology[0]))
+    }, [])
+
     return(
         <div>
-            <p><span>03</span>SPACE LAUNCH 101</p>
-            <div><img src='' alt="" /></div>
+            <p className={styles.title}><span>03</span>SPACE LAUNCH 101</p>
+            <div className={styles.image}>
+            {technology.images && <img className={styles.technologyImage} src={technology.images.landscape} alt="" width={375} height={170} />}
+            </div>
             <div>
                 <nav>
-                    <ul>
+                    <ul className={styles.listContainer}>
                         <li>
-                            <Link>1</Link>
+                            <Link className={styles.list}>1</Link>
                         </li>
                         <li>
-                            <Link>2</Link>
+                            <Link className={styles.list}>2</Link>
                         </li>
                         <li>
-                            <Link>3</Link>
+                            <Link className={styles.list}>3</Link>
                         </li>
                     </ul>
                 </nav>
-                <div>
-                    <p></p>
-                    <h2></h2>
-                    <p></p>
+                <div className={styles.descriptionContainer}>
+                    <p className={styles.heading}>THE TERMINOLOGY...</p>
+                    <h2 className={styles.headingName}>{technology.name}</h2>
+                    <p className={styles.description}>{technology.description}</p>
                 </div>
             </div>
         </div>
