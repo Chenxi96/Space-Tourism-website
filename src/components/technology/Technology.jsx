5ff6632de6f65b8a905671e02da5aa606df1d645
Chenxi <lin.chenxi14@gmail.com>
2024-03-04 20:53:46 -0500

    make the Link tag to indicate which route it is

--- src/components/technology/Technology.jsx
+++ src/components/technology/Technology.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import styles from './Technology.module.scss'
 
 export default function Technology() {
+    const technologyRoute = useParams();
     const [technology, setTechology] = useState({});
 
     useEffect(() => {
         fetch('../../../data.json')
             .then(res => res.json())
-            .then(data => setTechology(data.technology[0]))
-    }, [])
+            .then(data => {
+                if(location.pathname === '/Technology') {
+                    setTechology(data.technology[0])
+                } else {
+                    setTechology(data.technology[technologyRoute.technology])
+                }
+            })
+    }, [technologyRoute])
 
     return(
         <div>
@@ -21,13 +28,13 @@ export default function Technology() {
                 <nav>
                     <ul className={styles.listContainer}>
                         <li>
-                            <Link className={styles.list}>1</Link>
+                            <Link to={location.pathname === '/Technology' ? '0' : '../0'} className={location.pathname === '/Technology' || location.pathname === '/Technology/0' ? `${styles.isActive}` : `${styles.isPending}`}>1</Link>
                         </li>
                         <li>
-                            <Link className={styles.list}>2</Link>
+                            <Link to={location.pathname === '/Technology' ? '1' : '../1'} className={location.pathname === '/Technology/1' ? `${styles.isActive}` : `${styles.isPending}`}>2</Link>
                         </li>
                         <li>
-                            <Link className={styles.list}>3</Link>
+                            <Link to={location.pathname === '/Technology' ? '2' : '../2'} className={location.pathname === '/Technology/2' ? `${styles.isActive}` : `${styles.isPending}`}>3</Link>
                         </li>
                     </ul>
                 </nav>
